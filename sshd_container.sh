11d6794e9818145458950e38368718845bca4958
Victor Penso <vic.penso@gmail.com>
2021-07-28 14:58:34 +0200

    start to work on the scripts...

--- sshd_container.sh
+++ sshd_container.sh
@@ -1,17 +1,81 @@
+#!/usr/bin/env bash
+
+VERSION=0.1
+
+# Filename of this script
+SCRIPT=${0##*/}
+
+# Help text for this script
+HELP=\
+"usage: $SCRIPT [-h] [--version] [arg]
+Add more text here
+positional arguments:
+  arg                  describe the argument
+optional arguments:
+  -d, --debug          enable verbose output
+  -h, --help           show this help message
+  --version            program version number "
+
+
+function _debug() {
+        if [ "$SSHD_CONTAINER_DEBUG" = "true" ]; then
+                echo 1>&2 "Debug: $@"
+        fi
+}
+
+function _error() {
+        echo 1>&2 "Error: $@"
+	exit 1
+}
+
+SSHD_CONTAINER_CONFIG=/etc/default/sshd_container
+
+# Parse the command line options
+ARGS=$(getopt -o h -l "help,version" -- "$@")
+eval set -- "$ARGS"
+while true; do
+        case "$1" in
+        -d|--debug)
+                SSHD_CONTAINER_DEBUG=true
+                shift
+                ;;
+        -h|--help)
+                echo "$HELP"
+                exit 0
+                ;;
+        --version)
+                echo $VERSION
+                exit 0
+                ;;
+        --)
+                shift
+                break 
+                ;;
+        *) 
+                break 
+                ;;
+        esac
+done
+
 shell=$(getent passwd $USER | cut -d : -f 7)
 
-# no login to a container by default
-VAE_DEFAULT=none
 # load the container default configuration if present
-test -f /etc/default/sshd_container && source /etc/default/sshd_container
-# set the VAE 
-VAE=${VAE:-$VAE_DEFAULT}
-
-if [ "$VAE" == "none" ]
+if test -f $SSHD_CONTAINER_CONFIG
 then
-        $shell -l ${SSH_ORIGINAL_COMMAND:+-c "$SSH_ORIGINAL_COMMAND"}
+        source $SSHD_CONTAINER_CONFIG
 else
+        _debug "$SSHD_CONTAINER_CONFIG configuration file missing"
+fi
 
+SINGULARIY_CONTAINER=${SINGULARIY_CONTAINER:-none}
+
+if [ "$SINGULARITY_CONTAINER" == "none" ]
+then
+        echo "No singularity container defined"
+        $shell -l ${SSH_ORIGINAL_COMMAND:+-c "$SSH_ORIGINAL_COMMAND"}
+
+# if a singularity container is defined...
+else
         PS3="Choose VAE by number: "
         __menu=0
         if [ "$VAE" == "menu" ]
