60959dcbdf0ab044b1d0fab0b67461407360d446
Victor Penso <vic.penso@gmail.com>
2021-08-05 12:02:16 +0200

    simple command wrapper for testing...

--- test.sh
+++ test.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 
+run() {
+        echo SSHD_CONTAINER_DEBUG=$SSHD_CONTAINER_DEBUG
+        echo SINGULARITY_CONTAINER=$SINGULARITY_CONTAINER
+        echo $@
+        echo ---------
+        eval "$@"
+        echo
+}
+
 # change to 2222 if sshd runs on the default port
 port=2223
 
-#set -x
+# stop on a command failing
 set -e
 
 # identify the login environment...
 command='grep -i pretty /etc/os-release ; ps -p $(echo $PPID) -f --no-headers'
 
 # do not propagate the SINGULARITY_CONTAINER environment variable
-ssh -F ssh-config -p $port root@ssh-container -- "$command"
+run "ssh -F ssh-config -p $port root@ssh-container -- '$command'"
 
 # enable debugging of the login script
 export SSHD_CONTAINER_DEBUG=true
@@ -20,64 +29,50 @@ export SSHD_CONTAINER_DEBUG=true
 ssh_options='-F ssh-config -o SendEnv=SINGULARITY_CONTAINER -o SendEnv=SSHD_CONTAINER_DEBUG'
 
 # root login does not launch a container
-ssh $ssh_options -p $port root@ssh-container -- "$command"
+run "ssh $ssh_options -p $port root@ssh-container -- '$command'"
 
 # non existing container
 export SINGULARITY_CONTAINER='foo'
-ssh $ssh_options -p $port vagrant@ssh-container -- "$command"
+run "ssh $ssh_options -p $port vagrant@ssh-container -- '$command'"
 
 ##
 # default container
 #
 
 export SINGULARITY_CONTAINER='' # blank environment variable
-ssh $ssh_options -p $port vagrant@ssh-container -- "$command"
-
+run "ssh $ssh_options -p $port vagrant@ssh-container -- '$command'"
 export SINGULARITY_CONTAINER=   # empty environment variable
-ssh $ssh_options -p $port vagrant@ssh-container -- "$command"
-
+run "ssh $ssh_options -p $port vagrant@ssh-container -- '$command'"
 unset SINGULARITY_CONTAINER     # no environment variable
-ssh $ssh_options -p $port vagrant@ssh-container -- "$command"
-
-echo 0 | ssh $ssh_options -p $port vagrant@ssh-container -- cat
-
-scp -d $ssh_options -P $port vagrant@ssh-container:/bin/bash /tmp
-scp -d $ssh_options -P $port /bin/bash vagrant@ssh-container:/tmp
-
-rsync -v -e "ssh $ssh_options -p $port" /bin/bash vagrant@ssh-container:/tmp
-rsync -v -e "ssh $ssh_options -p $port" vagrant@ssh-container:/bin/bash /tmp
-
-sftp $ssh_options -P $port vagrant@ssh-container:/bin/bash /tmp
-sftp $ssh_options -P $port vagrant@ssh-container:/tmp <<< $'put /bin/bash'
+run "ssh $ssh_options -p $port vagrant@ssh-container -- '$command'"
+run "echo 0 | ssh $ssh_options -p $port vagrant@ssh-container -- cat"
+run "scp -d $ssh_options -P $port vagrant@ssh-container:/bin/bash /tmp"
+run "scp -d $ssh_options -P $port /bin/bash vagrant@ssh-container:/tmp"
+run "rsync -v -e 'ssh $ssh_options -p $port' /bin/bash vagrant@ssh-container:/tmp"
+run "rsync -v -e 'ssh $ssh_options -p $port' vagrant@ssh-container:/bin/bash /tmp"
+run "sftp $ssh_options -P $port vagrant@ssh-container:/bin/bash /tmp"
+run "sftp $ssh_options -P $port vagrant@ssh-container:/tmp <<< $'put /bin/bash'"
 
 ##
 # specific container
 #
 
 export SINGULARITY_CONTAINER=/tmp/centos7.sif
-
-
-ssh $ssh_options -p $port vagrant@ssh-container -- "$command"
-
-echo 0 | ssh $ssh_options -p $port vagrant@ssh-container -- cat
-
-scp -d $ssh_options -P $port vagrant@ssh-container:/bin/bash /tmp
-scp -d $ssh_options -P $port /bin/bash vagrant@ssh-container:/tmp
-
-rsync -v -e "ssh $ssh_options -p $port" /bin/bash vagrant@ssh-container:/tmp
-rsync -v -e "ssh $ssh_options -p $port" vagrant@ssh-container:/bin/bash /tmp
-
-sftp $ssh_options -P $port vagrant@ssh-container:/bin/bash /tmp
-sftp $ssh_options -P $port vagrant@ssh-container:/tmp <<< $'put /bin/bash'
+run "ssh $ssh_options -p $port vagrant@ssh-container -- '$command'"
+run "echo 0 | ssh $ssh_options -p $port vagrant@ssh-container -- cat"
+run "scp -d $ssh_options -P $port vagrant@ssh-container:/bin/bash /tmp"
+run "scp -d $ssh_options -P $port /bin/bash vagrant@ssh-container:/tmp"
+run "rsync -v -e 'ssh $ssh_options -p $port' /bin/bash vagrant@ssh-container:/tmp"
+run "rsync -v -e 'ssh $ssh_options -p $port' vagrant@ssh-container:/bin/bash /tmp"
+run "sftp $ssh_options -P $port vagrant@ssh-container:/bin/bash /tmp"
+run "sftp $ssh_options -P $port vagrant@ssh-container:/tmp <<< $'put /bin/bash'"
 
 ##
 # no container
 #
 
 export SINGULARITY_CONTAINER=none
-
-ssh $ssh_options -p $port vagrant@ssh-container -- "$command"
-
-echo 0 | ssh $ssh_options -p $port vagrant@ssh-container -- cat
+run "ssh $ssh_options -p $port vagrant@ssh-container -- '$command'"
+run "echo 0 | ssh $ssh_options -p $port vagrant@ssh-container -- cat"
 
 
